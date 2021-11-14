0644caff5ec8140f73de58d8684a24e0fd2da841
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-11-15 03:07:51 +0900

    feat: post comment in pull request

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 const supportedLangs = ['en', 'ja'] as const
 
@@ -47,6 +48,22 @@ Supported languages are: ${supportedLangs.map(l => {
     const passage = docs[Math.floor(Math.random() * docs.length)][inputLang]
     core.info(passage)
     core.setOutput('passage', passage)
+
+    const context = github.context
+    const pr = context.payload.pull_request
+    if (!pr) {
+      core.info('Comment was skipped because pull request does not exist.')
+      return
+    }
+    const token = core.getInput('repo-token')
+    const octokit = github.getOctokit(token)
+    const res = await octokit.rest.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pr.number,
+      body: passage
+    })
+    core.info(`comment url: ${res.data.html_url}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
